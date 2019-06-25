55aa4517ba5378ce5966ff4e83453b9fbe4b6c78
Søren Nørup <soeren96@hotmail.com>
2019-06-25 13:49:41 +0200

    updates for the manual input

--- src/Result.js
+++ src/Result.js
@@ -1,72 +1,116 @@
 Result = function(id,profile_id){
     this.id = id;
     this.profile_id = profile_id;
-    this.res_arra = ['result','result2','result3','result4','result5','result6'];
+    this.res_array = ['result','result2','result3','result4','result5','result6'];
     // Setting data in question, setting data 5 different places
     this.setData = function(){
-        this.resetResultView();
-        for( var i = 0; i < 6; i++ ){          
-          if( this.id == i + 1 ){  
-           this.storeResults( this.createResultKeys( i + 1 )[1],
-                              this.resultarray( document.forms[0] ),
-                              this.createResultKeys( i + 1 )[0],
-                              this.handleResults( document.forms[0] ) ); //store the data into the sessionstorage         
-           document.getElementById( this.res_arra[i] ).innerHTML = sessionStorage.getItem(this.createResultKeys(i + 1)[0]);    
-          }  
+         
+        var results = document.forms[0];
+       
+        document.getElementById('result').innerHTML = " ";
+
+        //var res_arra = ['result','result2','result3','result4','result5','result6'];
+        
+        var all_res = this.resultarray(results);
+        
+       
+        var res = this.calculateAverage(results);
+        
+        if(res.toString().length > 3){
+            res = res.toFixed(2);
         }
-    };
-    
-    this.resetResultView = function(){
-        document.getElementById( 'result' ).innerHTML = " ";
-    };
-    
-    this.handleResults = function( results ){ 
-        var res = this.calculateAverage( results );
-        res = this.limitTwoDecimal( res );
-        if(isNaN( res )|| res === undefined){
+       
+        if(isNaN(res)){
                     res = 0;
                  }
-            return res; 
-     };
-     
-     this.limitTwoDecimal = function( res ){
-        if( res.toString().length > 3 ){     
-            res = res.toFixed( 2 );
-            return res;
-        } else { return res;}
-     };
-     this.createResultKeys = function(keyNum){
+        else if(res === undefined){
+            res = 0;
+        }
+        var num_of_ids = 6;
+        
+        for(var i = 0; i < num_of_ids; i++){         
+         
+         if(this.id == i+1){
+            
+           // Create the data keys
+           var keyNum = i+1;
            var value_key = 'value'+keyNum+'id'+this.profile_id;
+           
            var all_value_key = 'values'+keyNum+'id'+sessionStorage.profileid;
-            return [value_key,all_value_key]; 
-     };
-     
-     this.storeResults = function( all_value_key, all_res, value_key, res ){
-           sessionStorage.setItem( all_value_key,all_res );
-           sessionStorage.setItem( value_key,res ); 
-           sessionStorage.setItem( value_key,res );
-     };
+           //store the data into the sessionstorage
+           sessionStorage.setItem(all_value_key,all_res);
+
+           sessionStorage.setItem(value_key,res); 
+
+           sessionStorage.setItem(value_key,res);
+           console.log(sessionStorage.getItem(value_key));
+           
+           document.getElementById(this.res_array[i]).innerHTML = sessionStorage.getItem(value_key);
+           
+         }
+        
+    }
+    };
+     // store the manual data and put it into the result table
+    this.setManualData = function() {
+        
+         
+         var results = document.forms[1];
+         var resarray = this.resultarray(results);
+         
+       for(var i = 0; i < 6; i++){         
+         var res = resarray[i];
+           // Create the data keys
+         var keyNum = i+1;
+         var value_key = 'value'+keyNum+'id'+this.profile_id;
+           
+         sessionStorage.setItem(value_key,res); 
+
+           //console.log(sessionStorage.getItem(value_key));
+           
+           document.getElementById(this.res_array[i]).innerHTML = sessionStorage.getItem(value_key);
+                       
+    }
+        
+    };
      // View stuff
-     this.setResultViewIds = function( ){
-              resultarray =  ['result','result2','result3','result4','result5','result6'];
+     this.setResultViewIds = function(){
                 var resstr = "<tr>";
-                for( var i = 0; i < resultarray.length; i++ ){
-                    resstr += '<td id = "'+resultarray[i]+'"></td>' ;
+                
+                for(var i = 0; i < this.res_array.length; i++){
+                    resstr += '<td id = "'+this.res_array[i]+'"></td>' ;
                 }
                 resstr += '</tr>';
                 return resstr;
             };
+    
+
            // set the result into the result divs
     this.setResultView = function(){
-                 var res_arra = ['result','result2','result3','result4','result5','result6'];
-                for(var i = 0; i < res_arra.length; i++){
+                for(var i = 0; i < this.res_array.length; i++){
                     var keyNum = i+1;
-                    var result = 'value'+keyNum+'id'+this.profile_id;
+                     var result = 'value'+keyNum+'id'+this.profile_id;
                      
-                    document.getElementById(this.res_arra[i]).innerHTML = sessionStorage.getItem(result);
+                    document.getElementById(this.res_array[i]).innerHTML = sessionStorage.getItem(result);
                 }
                 
         };
+          // insert the result directly, without the questions
+    this.insertResultDirect = function() {
+          
+            
+                for(var i = 0; i < this.res_array.length; i++){
+                    var keyNum = i+1;
+                     var result = 'value'+keyNum+'id'+this.profile_id;
+                     
+                    document.getElementById(this.res_array[i]).innerHTML =
+                    ' <input id = "score"  class = "score form-control form-control-lg" min = "0" max = "4"'+
+                             ' placeholder="0" size="2" step="0.1" autocomplete="off" value = "'+
+                           sessionStorage.getItem(result)+'" name = "manual-input"/>';
+                }
+        
+        
+    };
     
     this.calculateAverage = function (formObj){
                 var sum = 0;
@@ -77,10 +121,11 @@ Result = function(id,profile_id){
                     if(this.valueNotZero(formObj[i].value)){
                         num++;
                     }
-                } 
+                }
+                
                return sum/num;
             };
-            
+             // Calculate the average of the the inputs
             this.calculateAverageArray = function (array){
                 var sum = 0;
                 var num = 0;
@@ -94,7 +139,7 @@ Result = function(id,profile_id){
                 
                return sum/num;
             };
-            
+            // Create an array of the form object
     this.resultarray = function (formObj){
                 var res = [];
                  for(var i = 0; i < formObj.length-1; i++){
